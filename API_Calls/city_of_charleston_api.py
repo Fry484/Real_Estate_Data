ad6ec10446266a583b061311ce600755e960f8b2
Fry484 <jonathan.robert.fry@gmail.com>
2021-02-19 15:25:55 -0600

    Added city_of_charleston_api.py

--- API_Calls/city_of_charleston_api.py
+++ API_Calls/city_of_charleston_api.py
@@ -1,7 +1,15 @@
 import requests
+import json
+
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
 
 zoning_geojson_url = "https://opendata.arcgis.com/datasets/c4d4ce44d9394c578a2999637a54b77f_0.geojson"
 
 zoning_data = requests.get(zoning_geojson_url)
 
-zoning_data
+#jprint(zoning_data.json())
+
+print(zoning_data.json().keys())
+print(zoning_data.json()['features'][0])
